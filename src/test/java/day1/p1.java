24131f1d73ec4cb836b809d9c78d5b2fa744cca2
rehangujjar <rehanmehmood507@gmail.com>
2021-03-02 20:15:34 -0800

    first

---
+++ src/test/java/day1/p1.java
@@ -0,0 +1,15 @@
+package day1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class p1 {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver= new ChromeDriver();
+        driver.get("http://google.com");
+            driver.close();
+    }
+}

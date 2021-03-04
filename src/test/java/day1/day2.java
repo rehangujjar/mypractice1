8019abb1ce089a7e4db5c976431a5edbf18d8c32
rehangujjar <rehanmehmood507@gmail.com>
2021-03-03 22:02:38 -0800

    second2

--- src/test/java/day1/day2.java
+++ src/test/java/day1/day2.java
@@ -3,15 +3,15 @@ package day1;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+public class day2 {
 
-public class p1 {
 
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver= new ChromeDriver();
         driver.get("http://google.com");
-       // driver.navigate().to("http://amazon.com");
+        // driver.navigate().to("http://amazon.com");
         //System.out.println("fresh");
-           // driver.close();
+        // driver.close();
     }
 }
